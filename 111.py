0dfb11c4a66862b48c3d797b4193a1e49eea906e
SENSEN <sensen@SENSENdeMac-mini.local>
2026-05-01 20:22:18 +0800

    删除多个文件

--- 111.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open("data/laboratory.csv", "r") as f:
-    reader = csv.reader(f)
-    data = list(reader)
-print(len(data[0]))
--- 字段.py
+++
@@ -1,4 +0,0 @@
-import json
-with open('feature_columns.json') as f:
-    features = json.load(f)
-print(features)
